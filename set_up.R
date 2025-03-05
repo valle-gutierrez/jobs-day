f1a14f6858f62cb1edacde854cf06f5cc2ea25b2
valle-gutierrez <valle-gutierrez@tcf.org>
2025-03-05 16:37:29 -0500

    Add files via upload
    Uploading set up file for reference via url

---
+++ set_up.R
@@ -0,0 +1,12 @@
+# Universal set up 
+
+# Key Packages
+require (tidyverse)
+require (haven)
+require (readxl)
+require (ggplot2)
+require (ipumsr)
+require (spatstat)
+require (writexl)
+require (googlesheets4)
+require (devtools)
